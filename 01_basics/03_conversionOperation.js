54cb166b89c5af3c726219013220424cd30697fb
HarshakChandra01 <yharsh1235@gmail.com>
2025-01-23 08:00:13 +0000

    covertion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)//score is converted to number from string
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+//NaN not a number 33abc = NaN
+// for null = 0/
+// true = 1 ; false = 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 = true; 0 = false
+// "" = false
+// "harshak" = true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);

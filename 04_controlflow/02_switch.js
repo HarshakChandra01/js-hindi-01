1f7a5c58f3d25b4c5087de3dafd1e5efbdc88f9e
HarshakChandra01 <yharsh1235@gmail.com>
2025-03-20 17:44:36 +0000

    swi

---
+++ 04_controlflow/02_switch.js
@@ -0,0 +1,51 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");//as key is matched then also code will execute except default
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+
+// in case of string
+const month = "April"
+
+switch (month) {
+    case "Jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");//as key is matched then also code will execute except default
+        break;
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}

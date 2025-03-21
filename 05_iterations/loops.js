1fe11309710899e8e00e31ae2d9da987297196fc
HarshakChandra01 <yharsh1235@gmail.com>
2025-03-21 17:45:04 +0000

    loop

---
+++ 05_iterations/loops.js
@@ -0,0 +1,37 @@
+// for
+
+for (let i = 0; i <= 10; i++) {
+    const element =  i;
+    if(element == 5){
+        // console.log("5 is best number");
+    }
+    // console.log(element); 
+}
+
+// console.logelement();
+//as element is declayred outside scope so their will be no access out side scope
+
+for (let i = 0; i <= 10; i++) {
+    // console.log(`Outer loop value: ${i}`);
+    for (let j = 0; j <= 10; j++) {
+       // console.log(`Inner loop value ${j} and inner loop ${i}`);  
+    //    console.log(i + '*' + ' = ' + i*j);
+        
+    } 
+}
+
+let MyArray = ["flash", "batman", "superman"]
+// console.log(MyArray.length);
+for (let index = 0; index < MyArray.length; index++) {//<= undefined
+    const element = MyArray[index]; 
+    // console.log(element);
+      
+}
+
+// break and continue keywords
+
+for (let index = 0; index <=20index++) {
+    const element = array[index];
+    
+}
+

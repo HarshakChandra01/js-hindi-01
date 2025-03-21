a6278dd7394c9c8acec8987632774a66d7df64c1
HarshakChandra01 <yharsh1235@gmail.com>
2025-03-21 18:36:21 +0000

    do

---
+++ 05_iterations/dowhile.js
@@ -0,0 +1,22 @@
+let index = 1
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index = index + 2;
+}
+
+let myArray = ['flash', 'batman', 'superman']
+
+let arr = 0
+while (arr < myArray.length){
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1;
+}
+
+// ----------------do while----------------
+// This loop do the specialization
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++;// value is 11 will be printed but condition will check later
+} while (score <= 10);
